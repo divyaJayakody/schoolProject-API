01fda4f8c4ab8ce9081af4d06405958f131b985b
it17156134_malkini <it17156134@my.sliit.lk>
2021-02-05 05:38:13 +0530

    added front end dist

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/', express.static('public/schoolProject/'));
+app.use(express.static(path.join(__dirname, './public/dist/schoolProject')))
+// app.use(express.static(path.join(__dirname, 'public')));
 
 /* routing incoming requests to the controller */
 let schoolRouter = require('./routes/school-controller');
